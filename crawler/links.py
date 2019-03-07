c3c41fcd658d110de5830bea6e04ba66e3ada234
Jaka Stavanja <jakastavanja@gmail.com>
2019-03-07 19:03:07 +0100

    :globe_with_meridians: Added a Links file with simple URL utilities

---
+++ crawler/links.py
@@ -0,0 +1,42 @@
+"""
+This file contains utility functions for working with URLs.
+"""
+from urllib.parse import urlsplit
+import requests
+
+
+class Links:
+
+    @staticmethod
+    def sanitize(url):
+        """
+        Removes unnecessary symbols from the URL and fixes
+        any capitalization inconsistencies.
+
+        Example:
+
+        >>> url = 'HTTP://www.Python.org/doc/#'
+        >>> sanitize_url(url)
+        'http://www.Python.org/doc/'
+
+        HTTP gets fixed as capitalization there doesn't matter
+        whereas the actual URL's are case-sensitive.
+        """
+        return urlsplit(url).getUrl()
+
+    @staticmethod
+    def has_parsable_content(url):
+        """
+        Tells whether a URL leads to a webpage. E.g. if the 
+        URL leads to a .pptx file, we don't consider adding
+        it to the frontier.
+
+        Examples:
+
+        >>> Links.is_parsable('http://docs.python-requests.org/en/master/user/quickstart/#response-status-codes')
+        True
+
+        >>> Links.is_parsable('http://www.africau.edu/images/default/sample.pdf')
+        False
+        """
+        return 'text/html' in requests.head(url).headers['Content-Type']
