5eca14372d2b20ca31c0e2d7536b018ff328e0e4
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2019-03-07 12:14:52 +0100

    Fixed import linting issue

--- crawler/main.py
+++ crawler/main.py
@@ -2,7 +2,7 @@
 This is the crawler's entrypoint which should
 be used to run the whole crawling process.
 """
-from settings import SettingsReader
+from crawler.settings import SettingsReader
 
 if __name__ == "__main__":
     config = SettingsReader('./config.yaml').config
